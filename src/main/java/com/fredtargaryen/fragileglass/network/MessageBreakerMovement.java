efae4c3f97d7ffd8712b3d259df6c075a4186db9
Unknown <lggy@hotmail.co.uk>
2019-02-22 19:12:23 +0000

    Port network directory

--- src/main/java/com/fredtargaryen/fragileglass/network/MessageBreakerMovement.java
+++ src/main/java/com/fredtargaryen/fragileglass/network/MessageBreakerMovement.java
@@ -2,36 +2,31 @@ package com.fredtargaryen.fragileglass.network;
 
 import com.fredtargaryen.fragileglass.FragileGlassBase;
 import io.netty.buffer.ByteBuf;
-import net.minecraft.entity.player.EntityPlayerMP;
-import net.minecraft.util.IThreadListener;
-import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
-import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
-import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import net.minecraftforge.fml.network.NetworkEvent;
+
+import java.util.function.Supplier;
 
 /**
  * Used to handle player movement. See IPlayerBreakCapability, PlayerBreakStorage and PlayerBreakFactory for more
  * information.
  */
-public class MessageBreakerMovement implements IMessage, IMessageHandler<MessageBreakerMovement, IMessage>
+public class MessageBreakerMovement
 {
     public double motionx;
     public double motiony;
     public double motionz;
     public double speed;
 
-    @Override
-    public IMessage onMessage(final MessageBreakerMovement message, MessageContext ctx)
+    public void onMessage(Supplier<NetworkEvent.Context> ctx)
     {
-        final EntityPlayerMP player = ctx.getServerHandler().player;
-        final IThreadListener serverWorld = player.getServerWorld();
-        serverWorld.addScheduledTask(() -> {
-            player.getCapability(FragileGlassBase.PLAYERBREAKCAP, null).onMessage(message);
-        });
-        return null;
+        ctx.get().enqueueWork(() -> ctx.get().getSender().getCapability(FragileGlassBase.PLAYERBREAKCAP, null)
+                .ifPresent(pbc -> pbc.onMessage(this)));
     }
 
-    @Override
-    public void fromBytes(ByteBuf buf)
+    /**
+     * Effectively fromBytes from 1.12.2
+     */
+    public MessageBreakerMovement(ByteBuf buf)
     {
         this.motionx = buf.readDouble();
         this.motiony = buf.readDouble();
@@ -39,7 +34,6 @@ public class MessageBreakerMovement implements IMessage, IMessageHandler<Message
         this.speed = buf.readDouble();
     }
 
-    @Override
     public void toBytes(ByteBuf buf)
     {
         buf.writeDouble(this.motionx);
--- src/main/java/com/fredtargaryen/fragileglass/network/PacketHandler.java
+++ src/main/java/com/fredtargaryen/fragileglass/network/PacketHandler.java
@@ -1,16 +1,20 @@
 package com.fredtargaryen.fragileglass.network;
 
 import com.fredtargaryen.fragileglass.DataReference;
-import net.minecraftforge.fml.common.network.NetworkRegistry;
-import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
-import net.minecraftforge.fml.relauncher.Side;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.fml.network.NetworkRegistry;
+import net.minecraftforge.fml.network.simple.SimpleChannel;
 
-public class PacketHandler
-{
-    public static final SimpleNetworkWrapper INSTANCE = NetworkRegistry.INSTANCE.newSimpleChannel(DataReference.MODID);
+public class PacketHandler {
+    public static final SimpleChannel INSTANCE = NetworkRegistry.newSimpleChannel(
+            new ResourceLocation(DataReference.MODID, "channel"),
+            () -> "1.0", //version that will be offered to the server
+            (String s) -> s.equals("1.0"), //client accepted versions
+            (String s) -> s.equals("1.0"));//server accepted versions
 
     public static void init()
     {
-        INSTANCE.registerMessage(MessageBreakerMovement.class, MessageBreakerMovement.class, 0, Side.SERVER);
+        INSTANCE.registerMessage(0, MessageBreakerMovement.class, MessageBreakerMovement::toBytes, MessageBreakerMovement::new, MessageBreakerMovement::onMessage);
     }
 }
