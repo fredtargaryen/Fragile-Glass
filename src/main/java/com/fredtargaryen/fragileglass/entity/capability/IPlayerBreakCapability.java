43c8b81d38b5e2a59950258ee97b69d093187730
Unknown <lggy@hotmail.co.uk>
2019-06-26 18:34:10 +0100

    First 1.14 beta

--- src/main/java/com/fredtargaryen/fragileglass/entity/capability/IPlayerBreakCapability.java
+++ src/main/java/com/fredtargaryen/fragileglass/entity/capability/IPlayerBreakCapability.java
@@ -1,7 +1,5 @@
 package com.fredtargaryen.fragileglass.entity.capability;
 
-import com.fredtargaryen.fragileglass.network.MessageBreakerMovement;
-
 /**
  * Players get this Capability instead of IBreakCapability, because their motion is never updated server-side.
  * Player motion is set via a MessageBreakerMovement so this method is required.
--- src/main/java/com/fredtargaryen/fragileglass/network/MessageBreakerMovement.java
+++ src/main/java/com/fredtargaryen/fragileglass/network/MessageBreakerMovement.java
@@ -10,8 +10,7 @@ import java.util.function.Supplier;
  * Used to handle player movement. See IPlayerBreakCapability, PlayerBreakStorage and PlayerBreakFactory for more
  * information.
  */
-public class MessageBreakerMovement
-{
+public class MessageBreakerMovement {
     public double motionx;
     public double motiony;
     public double motionz;
@@ -28,16 +27,14 @@ public class MessageBreakerMovement
     /**
      * Effectively fromBytes from 1.12.2
      */
-    public MessageBreakerMovement(ByteBuf buf)
-    {
+    public MessageBreakerMovement(ByteBuf buf) {
         this.motionx = buf.readDouble();
         this.motiony = buf.readDouble();
         this.motionz = buf.readDouble();
         this.speed = buf.readDouble();
     }
 
-    public void toBytes(ByteBuf buf)
-    {
+    public void toBytes(ByteBuf buf) {
         buf.writeDouble(this.motionx);
         buf.writeDouble(this.motiony);
         buf.writeDouble(this.motionz);
